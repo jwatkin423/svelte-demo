d8252a8323bbdac28084857b2369559b6d8c9a84
Joseph Watkin <jwatkin@terradatum.com>
2020-05-11 16:26:42 -0400

    KMI updates for the double line graph

--- src/components/KMIMedium.svelte
+++ src/components/KMIMedium.svelte
@@ -4,7 +4,8 @@ import ChangeArrows  from './ChangeArrows.svelte';
 import {faCaretUp, faCaretDown, faCircle} from '@fortawesome/free-solid-svg-icons';
 
 export let data = [];
-export let reportPeriod = [];
+export let dataTwo = []
+export let reportPeriod;
 export let year;
 export let searchType;
 
@@ -15,8 +16,8 @@ export let s_color = false;
 // primary color
 let primary_fill_color = '';
 let secondary_fill_color = '';
-$: primary_fill_color = p_color ? p_color : '#019184';
-$: secondary_fill_color = s_color ? s_color :' #666666';
+$: primary_fill_color = p_color;
+$: secondary_fill_color = s_color;
 
 export let showDollar;
 $: dollar = showDollar ? '$' : '';
@@ -27,22 +28,33 @@ let icon = [faCaretUp, faCaretDown, faCircle];
 
 let count = [];
 let change = 'NA';
-let mthChange = 'NA';
-let ytdChange = 'NA';
+let chnageTwo = 'NA';
 let lastMonth = 1.0;
+let lastMonthTwo = 1.0;
 let initialMonth = 0.0;
+let initialMonthTwo = 0.0;
 
+// first data set
 $: toFixed = chartType === 'spOpRatio' ? 2 : 1;
 $: initialMonth = parseFloat(data[0]);
 $: lastMonth = data[data.length - 1];
 $: change = (lastMonth - initialMonth).toFixed(toFixed);
-$: monthTwo = data[2];
+$: chngPercent = changePercentage(change, initialMonth);
+
+
+// second second data set
+$: initialMonthTwo = (chartType === 'saleMedianSoldMedian' || chartType === 'supplyDemand') ? parseFloat(dataTwo[0]) : 0;
+$: lastMonthTwo = (chartType === 'saleMedianSoldMedian' || chartType === 'supplyDemand') ? dataTwo[dataTwo.length - 1] : 0;
+$: changeTwo = (chartType === 'saleMedianSoldMedian' || chartType === 'supplyDemand') ? (lastMonthTwo - initialMonthTwo).toFixed(toFixed) : 0;
+$: chngPercentTwo = (chartType === 'saleMedianSoldMedian' || chartType === 'supplyDemand') ? changePercentage(changeTwo, initialMonthTwo) : 0;
+
+
+// month year-to-date
 $: yearOne = data[1];
 $: yearTwo = data[3];
+$: monthTwo = data[2];
 $: mthChange = (monthTwo - initialMonth).toFixed(toFixed);
-$: chngPercent = changePercentage(change, initialMonth);
 $: ytdChange = (data[3] - data[1]).toFixed(toFixed);
-
 $: percentMnthChange = mnthYtdChangePercentage(mthChange, initialMonth);
 $: percentYtdChange = mnthYtdChangePercentage(ytdChange, yearOne);
 
@@ -192,6 +204,57 @@ function formatLastPeriod(period) {
     return newPeriod;
 }
 
+function rowType(ct, row) {
+
+    if (row === 1) {
+        if(ct === 'supplyDemand') {
+            return 'Supply';
+        } else {
+            return 'For Sale';
+        }
+    }
+    
+    if (row === 2) {
+        if(ct === 'supplyDemand') {
+            return 'Demand';
+        } else {
+            return 'Sold';
+        }
+    }
+}
+
+let showChart = true;
+
+function toggleDisplayChart() {
+    showChart = showChart === true ? false : true;
+    let chart = document.querySelector('.chart');
+    if (chart) {
+        if (!showChart) {
+            chart.style.display = 'none';
+        } else {
+            chart.style.display = 'block';
+        }
+    }
+
+
+}
+
+let showTable = true;
+
+function toggleDisplayTable() {
+    showTable = showTable === true ? false : true;
+    let table = document.querySelector('.tabular-wrapper');
+    if (table) {
+        if (!showTable) {
+            table.style.display = 'none';
+        } else {
+            table.style.display = 'block';
+        }
+    }
+
+
+}
+
 </script>
 
 <style>
@@ -199,6 +262,22 @@ function formatLastPeriod(period) {
         display: block;
         text-align: center;
         background-color: #ffffff;
+        width: 80%;
+    }
+
+    .button-wrapper {
+        width: 10%;
+        display: table-cell;
+        line-height: 60px;
+        vertical-align: bottom;
+    }
+
+    button {
+        height: 20px;
+    }
+
+    .div-buffer {
+        width: 10%;
     }
 
     table {
@@ -226,6 +305,18 @@ function formatLastPeriod(period) {
         padding-top: 12px;
     }
 
+    .th-double-kmi {
+        height: 10px;
+        line-height: 10px;
+        font-size: 10px;
+        font-weight: 800;
+        width: 108px;
+        color: #666666;
+        text-align: center;
+        padding-bottom: unset !important;
+        padding-top: 5px;
+    }
+
     .th-mnth-ytd {
         height: 12px;
         line-height: 12px;
@@ -250,8 +341,11 @@ function formatLastPeriod(period) {
         vertical-align: unset;
     }
 
-    .chng {
-        font-size: 16px;
+    .td-double-kmi {
+        height: 14px !important;
+        line-height: 14px !important;
+        font-size: 14px !important;
+        width: 108px;
     }
 
     .td-mnth-ytd {
@@ -261,11 +355,18 @@ function formatLastPeriod(period) {
         width: 125px;
     }
 
+    .td-lead {
+        text-align: right;
+    }
+
 </style>
+
+<div class="div-buffer">
+</div>
 
 <div class="table-wrapper">
     {#if data.length !== 0 && searchType !== 'mnth-ytd'}
-    
+        {#if chartType !== 'saleMedianSoldMedian' && chartType !== 'supplyDemand'}
         <table>
             <thead>
                 <tr>
@@ -286,7 +387,7 @@ function formatLastPeriod(period) {
                     {/if}
                     <td>
                         {#if chngPercent !== 'NA' && chngPercent != 0}
-                            {#if change > 0}
+                            {#if change >= 0}
                                 {chngPercent}%
                                 <ChangeArrows change={change} />
                             {:else}
@@ -302,6 +403,83 @@ function formatLastPeriod(period) {
                 </tr>
             </tbody>
         </table>
+        {:else}
+            <table class="table-double-kmi">
+                <thead>
+                    <tr>
+                        <th class="th-double-kmi"></th>
+                        <th class="th-double-kmi">{formatMonth(reportPeriod[0])}</th>
+                        <th class="th-double-kmi">{formatLastPeriod(reportPeriod[reportPeriod.length - 1])}</th>
+                        <th class="th-double-kmi">Change</th>
+                        <th class="th-double-kmi">% Change</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- First data set -->
+                    <tr>
+                        <td class="td-double-kmi td-lead">
+                            <i>
+                                <Icon customColor={primary_fill_color} class="year" tempId="line-one" icon={icon[2]} />
+                            </i>
+                            {rowType(chartType, 1)}
+                            </td>
+                        <td class="td-double-kmi" id="inital-month">{ formatNumber(initialMonth) }</td>
+                        <td class="td-double-kmi" id="last-month">{ formatNumber(lastMonth) } </td>
+                        {#if change >= 0}
+                            <td class="td-double-kmi" id="change">{ formatNumber(change) } <ChangeArrows change={change} /></td>
+                        {:else}
+                            <td class="td-double-kmi" id="change">({ formatNumber(change) }) <ChangeArrows change={change} /></td>
+                        {/if}
+                        <td class="td-double-kmi">
+                            {#if chngPercent !== 'NA' && chngPercent != 0}
+                                {#if change >= 0}
+                                    {chngPercent}%
+                                    <ChangeArrows change={change} />
+                                {:else}
+                                    ({chngPercent.toString().replace(/\-/, '')}%)
+                                    <ChangeArrows change={change} />
+                                {/if}
+                            {:else if chngPercent == 0}
+                                0%
+                            {:else if chngPercent === 'NA'}
+                                NA
+                            {/if}
+                        </td>
+                    </tr>
+                    <!-- Second data set -->
+                    <tr>
+                        <td class="td-double-kmi td-lead">
+                            <i>
+                                <Icon customColor={secondary_fill_color} class="year" tempId="line-two" icon={icon[2]} />
+                            </i>
+                            {rowType(chartType, 2)}
+                        </td>
+                        <td class="td-double-kmi" id="inital-month">{ formatNumber(initialMonthTwo) }</td>
+                        <td class="td-double-kmi" id="last-month">{ formatNumber(lastMonthTwo) } </td>
+                        {#if changeTwo >= 0}
+                            <td class="td-double-kmi" id="change">{ formatNumber(changeTwo) } <ChangeArrows change={changeTwo} /></td>
+                        {:else}
+                            <td class="td-double-kmi" id="change">({ formatNumber(changeTwo) }) <ChangeArrows change={changeTwo} /></td>
+                        {/if}
+                        <td class="td-double-kmi">
+                            {#if chngPercentTwo !== 'NA' && chngPercentTwo != 0}
+                                {#if changeTwo >= 0}
+                                    {chngPercentTwo}%
+                                    <ChangeArrows change={changeTwo} />
+                                {:else}
+                                    ({chngPercentTwo.toString().replace(/\-/, '')}%)
+                                    <ChangeArrows change={changeTwo} />
+                                {/if}
+                            {:else if chngPercentTwo == 0}
+                                0%
+                            {:else if chngPercentTwo === 'NA'}
+                                NA
+                            {/if}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        {/if}
     {:else}
         <div class="div-mnth-ytd">
             <table class="table-mnth-ytd">
@@ -327,8 +505,8 @@ function formatLastPeriod(period) {
                             0        
                         {/if}
                         <td class='td-mnth-ytd percent-change'>
-                            {#if percentMnthChange !== 'NA' && percentMnthChange != 0}
-                                {#if mthChange >= 0}
+                            {#if percentMnthChange !== 'NA' && percentMnthChange != 0 && percentMnthChange != 0.0}
+                                {#if mthChange > 0}
                                     {percentMnthChange}%
                                     <ChangeArrows change={mthChange} />
                                 {:else}
@@ -354,13 +532,13 @@ function formatLastPeriod(period) {
                             0    
                         {/if}
                         <td class='td-mnth-ytd percent-change'>
-                            {#if percentYtdChange !== 'NA' && percentYtdChange != 0}
-                                {#if ytdChange >= 0}
+                            {#if percentYtdChange !== 'NA' && percentYtdChange != 0 && percentYtdChange != 0.0}
+                                {#if ytdChange > 0}
                                     {percentYtdChange}%
                                     <ChangeArrows change={ytdChange} />
                                 {:else}
-                                    ({percentYtdChange}%)
-                                    <ChangeArrows change={ytdChange.toString().replace(/\-/, '')} />
+                                    ({percentYtdChange.toString().replace(/\-/, '')}%)
+                                    <ChangeArrows change={ytdChange} />
                                 {/if}
                             {:else if percentYtdChange == 0}
                                     0%
@@ -373,4 +551,8 @@ function formatLastPeriod(period) {
             </table>
         </div>
     {/if}
+</div>
+<div class="button-wrapper">
+    <a on:click|preventDefault on:click={toggleDisplayChart} href='.'><img src='/images/chart.png' width="20" height="20" alt='chart toggle' /></a>
+    <a on:click|preventDefault on:click={toggleDisplayTable} href='.'><img src='/images/table.png' width="20" height="20" alt='table toggle' /></a>
 </div>
