0efe4fffb08bf3a27bf5024e773c19b024cd651c
Joseph Watkin <jwatkin@terradatum.com>
2020-07-23 16:14:30 -0400

    New chart files

---
+++ src/charts/MnthYtdRight.svelte
@@ -0,0 +1,362 @@
+<script>
+import { scaleLinear } from 'd3-scale';
+import * as d3 from 'd3';
+import { onMount, afterUpdate } from 'svelte';
+import clearData from '../helpers/clear-chart';
+import { resetChart } from '../helpers/chartreset';
+
+// data for the chart
+export let dataTwo = [];
+export let reportPeriod = [];
+
+// set the chart width
+export let screenSize = '';
+
+// primary color prop
+export let p_color = false;
+export let s_color = false;
+
+// primary color
+let primary_fill_color = '';
+let secondary_fill_color = '';
+$: primary_fill_color = p_color ? p_color : '#019184';
+$: secondary_fill_color = s_color ? s_color :' #666666';
+
+export let showDollar;
+let dollar = '';
+$: dollar = showDollar ? '$' : '';
+
+const barWdith = 40;
+const barDistanceSetOne = .2;
+const barDistanceSetTwo = .65;
+
+let points = [];
+$: points = [...dataTwo];
+
+// $: drawChart(points);
+let margin = {top: 35, right: 20, bottom: 35, left: 20};
+let width = (screenSize / 2) - margin.left - margin.right;
+let height = 474 - margin.top - margin.bottom;
+
+$: width = (screenSize / 2) - margin.left - margin.right;
+$: drawChart(width);
+$: console.log(height);	
+
+let xTicks = [];
+let tmpDate;
+
+// draw the chart
+function drawChart() {
+
+	d3.selectAll("#chart-right > *").remove();
+    
+    width = (screenSize / 2) - margin.left - margin.right;
+    
+	// x ticks for the mnth ytd chart
+	xTicks[0] = reportPeriod[0];
+	xTicks[1] = 'YTD';
+
+	// svg for d3
+	let svgRight = d3.select(".median-chart-right")
+			.attr("width",width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom);
+
+	// ensures the highest tick mark value is greater than
+	// the highest value in the dataset
+	let maxHeight = d3.max(dataTwo, function(d){return Math.abs(d) * 1.1});
+	
+	// determine the min point 
+	// for the y scale (0 or negative)
+	let min = 0;
+	if (d3.min(points) < 0 ) {
+		min = -maxHeight;
+	}
+
+	// setup y scale
+	let yScale = d3.scaleLinear()
+					.domain([maxHeight, min])
+					.nice(8)
+					.range([0, height]);
+
+	// grid lines for the chart
+	let linesYaxis = d3.axisLeft(yScale)
+		.ticks(8)
+		.tickFormat("");
+
+	// add grid lines
+	svgRight.append("g")
+		.attr("class", "grid-right")
+		.attr("transform", "translate(0, " + margin.top + ")")
+		.call(linesYaxis.ticks(8))
+		.select('.domain').remove();
+
+	//add y axis
+	let yAxis = d3.axisLeft(yScale)
+		.ticks(8)
+		.tickSize(-width + margin.right)
+		.tickFormat(function(d) {
+			return formatYvalue(d);
+		});
+
+	svgRight.append("g")
+		.attr('class', "med-axis-tick-mark-right")
+		.attr("x", "0")
+		.attr("x1", "0")
+		.attr("x2", "0")
+		.attr("transform", "translate(0, " + margin.top + ")")
+		.call(yAxis.ticks(8)).attr("dx", "0")
+		.select('.domain').remove();
+
+        // set the y axis to 30 pixles out
+		svgRight.selectAll(".med-axis-tick-mark-right text")
+			.attr("x", "30");
+
+		svgRight.selectAll(".grid-right line").attr("x1", "60").attr("x2", width);
+	  
+
+	let bars = svgRight.selectAll("rect").data(dataTwo).enter().append("rect");
+	bars.attr("x",function(d,i) {
+		if (i === 0) {
+				return (width * barDistanceSetOne) + 10;
+		}
+		if (i === 1) {
+				return (width * barDistanceSetOne) + 60;
+		}
+		if (i == 2) {
+				return (width * barDistanceSetTwo) + 10;
+		}	
+		if (i == 3) {
+				return (width * barDistanceSetTwo) + 60;
+		}
+	})
+	.attr("y",function(d) {
+		if (min < 0) {
+			if(d < 0) {
+				return height/2 + margin.top;
+			}
+			else{
+				return yScale(d) + margin.top; 
+			}
+		} else {
+			return yScale(d) + margin.top;
+		}
+		
+	})//for bottom to top
+	.attr("width", barWdith)
+	.attr("height", function(d) {
+		if (min < 0) {
+			return height/2 -yScale(Math.abs(d));
+		}
+    	return height -yScale(Math.abs(d));
+		
+	});
+
+	bars.attr("fill",function(d, i) {
+		if(i===0 || i === 2){
+			return primary_fill_color;
+		}
+		else{
+			return secondary_fill_color;
+		}
+	});
+	
+	//add tag to every bar
+	let tags = svgRight.selectAll('.tags-right')
+		.data(dataTwo)
+		.enter()
+		.append("text")
+		.text(function(d) {
+			return formatTags(d);
+		})
+		.attr("x", function(d,i){
+			if (i == 0) {
+				return (width * barDistanceSetOne) + 25;
+			}
+			if (i == 1) {
+				return (width * barDistanceSetOne) + 80;
+			}
+			if (i == 2) {
+				return (width * barDistanceSetTwo) + 30;
+			}	
+			if (i == 3) {
+				return (width * barDistanceSetTwo) + 80;
+			}
+		})
+		.attr("y",function(d, i) {
+			if (d >= 0) {
+				return yScale(d) + margin.top - 5;
+			} else {
+				return height - yScale(Math.abs(d)) + margin.top + 15;
+			}
+		})
+		.attr('class', 'tags-right');
+
+	// add the x axis
+	let xAxis = svgRight.selectAll('.xaxis-ticks-right')
+		.data(xTicks)
+		.enter()
+		.append("text")
+		.text(function (d,i) {
+			if (i === 0) {
+				let chunks = d.split('<br>');
+				return chunks[0];
+			} else {
+				return d;
+			}
+		})
+		.attr("x", function(d, i) {
+			if (i === 0) {
+				return (width * barDistanceSetOne) + 55;
+			} else if(i === 1){			
+				return (width * barDistanceSetTwo) + 55;
+			}
+		})
+		.attr("class", "xaxis-ticks-right")
+		.attr("y", height + (margin.bottom / 2) + margin.top);
+
+	// add styles to the 0 tick and corresponding
+	// grid line
+	let lines = svgRight.selectAll('.grid-right .tick line')
+		.each(function(d) {
+			if (d == 0) {
+				d3.select(this).attr("class", "line-0");
+			}
+		});
+
+	let yTicksText = svgRight.selectAll('.med-axis-tick-mark-right .tick text')
+		.each(function(d) {
+			if (d == 0) {
+				d3.select(this).attr("class", "text-0").style("fill", "#666666");
+			} else {
+				d3.select(this).attr("class", "non-zero");
+			}
+		});
+
+}
+
+// format y value ticks
+function formatYvalue(d) {
+	let val = Math.abs(d);
+
+	let flag = 0;
+	let yValue = '';
+
+	if(d < 0) {
+		flag = 1;
+	}
+
+	if (val < 1000) {
+		yValue = val;
+	}
+
+	// if the val gte 1,000 and lt 1,000,000
+	if(val >= 1000 && val < 1000000) {
+		val /= 1000;
+		yValue = val.toString();
+		yValue = dollar + yValue + 'K';
+	}
+
+	// if the val gte 1,000,000 and lt 1,000,000,000
+	if(val >= 1000000 && val < 1000000000) {
+		val /= 1000000;
+		yValue = val.toString();
+		yValue = dollar + yValue + 'M';
+	}
+
+	// if the val gte 1,000,000,000 and lt 1,000,000,000,000
+	if(val >= 1000000000 && val < 1000000000000) {
+		val /= 1000000000;
+		yValue = val.toString();
+		yValue = dollar + yValue + 'B';
+	}
+	
+	if(flag) {
+		yValue = "(" + yValue + ")";
+	}
+
+	return yValue;
+}
+
+// format the bar values
+function formatTags(d) {
+	let val = Math.abs(d);
+
+	let flag = 0;
+	let yValue = '';
+
+	if(d < 0) {
+		flag = 1;
+	}
+
+	if (val < 1000) {
+		yValue = val;
+	}
+
+	// if the val gte 1,000 and lt 1,000,000
+	if(val >= 1000 && val < 1000000) {
+		val = Math.ceil(val / 1000);
+		val = val.toFixed(0);
+		yValue = val.toString();
+		yValue = dollar + yValue + 'K';
+	}
+
+	// if the val gte 1,000,000 and lt 1,000,000,000
+	if(val >= 1000000 && val < 1000000000) {
+		val = Math.ceil(val / 1000000);
+		val = val.toFixed(0);
+		yValue = val.toString();
+		yValue = dollar + yValue + 'M';
+	}
+
+	// if the val gte 1,000,000,000 and lt 1,000,000,000,000
+	if(val >= 1000000000 && val < 1000000000000) {
+		val = Math.ceil(val / 1000000000);
+		val = val.toFixed(0);
+		yValue = val.toString();
+		yValue = dollar + yValue + 'B';
+	}
+	
+	if(flag) {
+		yValue = "(" + yValue + ")";
+	}
+
+	return yValue;
+}
+
+afterUpdate(() => {
+	if(points.length > 0) {
+		drawChart();
+	}
+});
+
+</script>
+
+<style>
+	.chart {
+		margin-top: 0px;
+		background-color: #ffffff;
+		display: block;
+		/* margin: auto; */
+	}
+    .chart-right {
+		margin-left: 5px;
+		float: right;
+		background-color: #ffffff;
+    }
+    
+     svg {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+</style>
+
+<div class="chart-right">
+    <div class="chart">
+        <svg class="median-chart-right" id='chart-right'></svg>
+    </div>
+</div>
